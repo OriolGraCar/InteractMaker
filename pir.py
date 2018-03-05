0f464eaf1d3ed3642801bd197dee01d49186d285
AlvaroSmorras <35376500+AlvaroSmorras@users.noreply.github.com>
2018-03-05 12:11:12 +0100

    Add files via upload

--- pir.py
+++ pir.py
@@ -1,35 +1,30 @@
-from PDB4 import ProteinStructure as PS
-
-
 def superimpose_to_pir(finalpdb,pdb_used,outputfile):
     """
     :param finalpdb: pdb object of the final structure
     :param pdb_used: list of tuples with the pdb file name in the first and in the second a string with the names
-                     of the chains in the final pdb.
+                     of the chains in the final pdb. Or a list of pdb object.
     :param outputfile: name to write the pir alignment
     :return: pir format alignment file"""
 
     pir_file = open(outputfile, 'w')
     pir_file.write(">%s" % finalpdb.get_id())
     chains_order = []
-    for chain in finalpdb.getchains():
+    for chain in finalpdb:
         chain_id = chain.get_id()
         chain_seq = chain.get_sequence_str()
-        chains_order.append(chain_id, len(chain))
+        chains_order.append((chain_id, len(chain)))
         pir_file.write("\n%s/" % chain_seq)
 
-    pir_file.write("*\n")
+    pir_file.write("*")
 
     for pdb in pdb_used:
-        current_pdb = PS("PR", pdb[0])
-        pir_file.write(">%s" % current_pdb.get_id())
-        chains = current_pdb.getchains()
-        placed_chains = 0
+        pir_file.write("\n>%s" % pdb.get_id())
         for i in range(len(chains_order)):
-            if chains_order[i][0] == pdb[1][placed_chains]:
-                pir_file.write("\n%s/" % chains[placed_chains].get_sequence_str())
+            if chains_order[i][0] in pdb.child_dict:
+                pir_file.write("\n%s/" % pdb.child_dict[chains_order[i][0]].get_sequence_str())
             else:
                 string_to_write = '-' * chains_order[i][1]
                 pir_file.write("\n%s" % string_to_write)
 
     pir_file.write("*\n")
+    pir_file.close()
