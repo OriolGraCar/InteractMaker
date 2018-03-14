fc4d207ff010a105310b26b08da5e9156ece5ff2
AlvaroSmorras <35376500+AlvaroSmorras@users.noreply.github.com>
2018-03-14 18:46:05 +0100

    Add files via upload

--- visual.py
+++ visual.py
@@ -2,13 +2,13 @@ import tkinter
 import pymol
 from pymol import cmd
 
-pymol.finish_launching(['pymol', 'qc'])
+pymol.finish_launching(['pymol', '-qc'])
 
 def photo_pdb(pdb_file, out_file):
     cmd.reinitialize()
     cmd.load(pdb_file)
     cmd.show_as('cartoon', 'all')
     cmd.util.cbc()
-    cmd.png(filename=out_file, width=1024, height=926, ray=0)
+    cmd.png(filename=out_file, width=1024, height=926, ray=0, dpi=300)
 
 photo_pdb('pdb/vira.pdb', 'pdb/vira.png')
